b2c7da1454cd0e0439d0508b2e353cc73a9e2466
Dima <uvarov3123@gmail.com>
2023-11-18 13:36:24 +0200

    add header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,16 +3,37 @@ import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
 
-// window.addEventListener('load', pageLoad)
+window.addEventListener('load', pageLoad)
 
-// function pageLoad() {
-//     const htmlTag = document.documentElement
+function pageLoad() {
+    const htmlTag = document.documentElement
 
-//     document.addEventListener('click', e => {
-//         const targetElement = e.target
+    document.addEventListener('click', e => {
+        const targetElement = e.target
 
-//         if (targetElement.closest('.class')) {
-//             console.log('1');
-//         }
-//     })
-// }
+
+        if (!targetElement.closest('.item-header__content.open') && document.querySelector('.item-header__content.open')) {
+            document.querySelector('.item-header__content.open').classList.remove('open')
+        }
+
+        if (targetElement.closest('.item-header__content') && !targetElement.closest('.item-header__list')) {
+            if (targetElement.closest('a')) {
+                e.preventDefault()
+            }
+
+            targetElement.closest('.item-header__content').classList.toggle('open')
+
+        }
+
+        ////
+
+        if (!targetElement.closest('.menu__item.open') && document.querySelector('.menu__item.open')) {
+            document.querySelector('.menu__item.open').classList.remove('open')
+        }
+
+        if (targetElement.closest('.menu__item')) {
+            targetElement.closest('.menu__item').classList.toggle('open')
+        }
+
+    })
+}
